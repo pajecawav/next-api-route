3271e5afe303041ea5708391cdc8c8411a61c11e
pajecawav <pajecawav@gmail.com>
2023-01-29 22:11:23 +0300

    do not check formatting for yaml files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-	"*.{js,jsx,ts,tsx,cjs,cts,json,md,yml,yaml,css}": filenames =>
+	"*.{js,jsx,ts,tsx,cjs,cts,json,md,yml,css}": filenames =>
 		`prettier --write ${filenames.map(filename => `'${filename}'`).join(" ")}`,
 	"src/**/*.{js,jsx,ts,tsx}": () => "pnpm lint:tsc",
 };
